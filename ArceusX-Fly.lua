3959741278fd2b2b09f3e5a2788c322c8170e711
Riky47 <56732510+Riky47@users.noreply.github.com>
2023-02-07 18:28:46 +0100

    Update ArceusX-Fly.lua

--- ArceusX-Fly.lua
+++ ArceusX-Fly.lua
@@ -2,140 +2,295 @@
 		MOBILE FLYING SCRIPTS
 		Original: me_ozoneYT & Lucifear
 		
-		Rewrited and optimized by Riky47#3355 for Arceus X
+		Remade and optimized by Riky47#3355 for Arceus X
 		# spdmteam.com
 ]]
+
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local Arceus = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local Intro = Instance.new("Frame")
+local UICorner_2 = Instance.new("UICorner")
+local Logo = Instance.new("ImageLabel")
+local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+local Title = Instance.new("TextLabel")
+local Menu = Instance.new("ScrollingFrame")
+local UIListLayout = Instance.new("UIListLayout")
+local Toggle = Instance.new("ImageButton")
+local UICorner_3 = Instance.new("UICorner")
+local Enabled = Instance.new("Frame")
+local UIAspectRatioConstraint_2 = Instance.new("UIAspectRatioConstraint")
+local UICorner_4 = Instance.new("UICorner")
+local Check = Instance.new("Frame")
+local UIAspectRatioConstraint_3 = Instance.new("UIAspectRatioConstraint")
+local UICorner_5 = Instance.new("UICorner")
+local Name = Instance.new("TextLabel")
+local UIGradient = Instance.new("UIGradient")
+local Button = Instance.new("ImageButton")
+local UICorner_6 = Instance.new("UICorner")
+local Name_2 = Instance.new("TextLabel")
+local UIGradient_2 = Instance.new("UIGradient")
+local tab = Instance.new("Frame")
+
+--Properties:
+
+Arceus.Name = "Arceus"
+Arceus.Enabled = true
+Arceus.ResetOnSpawn = true
+Arceus.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Arceus.DisplayOrder = 999999999
+
+Main.Name = "Main"
+Main.Parent = Arceus
+Main.Active = true
+Main.Draggable = true
+Main.AnchorPoint = Vector2.new(0.5, 0.5)
+Main.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+Main.BorderSizePixel = 0
+Main.Position = UDim2.new(0.5, 0, -0.15, 0) --UDim2.new(0.5, 0, 0.5, 0)
+Main.Size = UDim2.new(0.3, 0, 0.3, 0)
+
+UICorner.CornerRadius = UDim.new(0.1, 0)
+UICorner.Parent = Main
+
+Intro.Name = "Intro"
+Intro.Parent = Main
+Intro.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+Intro.ClipsDescendants = true
+Intro.Size = UDim2.new(1, 0, 1, 0)
+Intro.ZIndex = 2
+
+UICorner_2.CornerRadius = UDim.new(0.1, 0)
+UICorner_2.Parent = Intro
+
+Logo.Parent = Intro
+Logo.AnchorPoint = Vector2.new(0.5, 0.5)
+Logo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Logo.BackgroundTransparency = 1.000
+Logo.BorderSizePixel = 0
+Logo.Position = UDim2.new(0.5, 0, 0.5, 0)
+Logo.Size = UDim2.new(0.75, 0, 0.75, 0)
+Logo.ZIndex = 2
+Logo.Image = "http://www.roblox.com/asset/?id=9178187770"
+Logo.ScaleType = Enum.ScaleType.Fit
+
+UIAspectRatioConstraint.Parent = Logo
+
+Title.Name = "Title"
+Title.Parent = Main
+Title.AnchorPoint = Vector2.new(1, 0)
+Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Title.BackgroundTransparency = 1.000
+Title.BorderSizePixel = 0
+Title.Position = UDim2.new(0.975, 0, 0.075, 0)
+Title.Size = UDim2.new(0.85, 0, 0.155, 0)
+Title.Font = Enum.Font.TitilliumWeb
+Title.FontFace = Font.new("rbxasset://fonts/families/TitilliumWeb.json", Enum.FontWeight.Bold)
+Title.Text = "Arceus X | Mobile fly"
+Title.TextColor3 = Color3.fromRGB(255, 255, 255)
+Title.TextScaled = true
+Title.TextSize = 14.000
+Title.TextWrapped = true
+Title.TextXAlignment = Enum.TextXAlignment.Left
+Title.TextYAlignment = Enum.TextYAlignment.Center
+
+Menu.Name = "Menu"
+Menu.Parent = Main
+Menu.Active = true
+Menu.AnchorPoint = Vector2.new(0.5, 1)
+Menu.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Menu.BackgroundTransparency = 1.000
+Menu.AutomaticCanvasSize = Enum.AutomaticSize.Y
+Menu.BorderSizePixel = 0
+Menu.Position = UDim2.new(0.5, 0, 0.95, 0)
+Menu.Size = UDim2.new(0.95, 0, 0.65, 0)
+Menu.CanvasSize = UDim2.new(0, 0, 0, 0)
+Menu.ScrollBarThickness = Menu.AbsoluteSize.X/25
+
+UIListLayout.Parent = Menu
+--UIListLayout.Padding = UDim.new(0.025, 0)
+UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+
+Toggle.Name = "Toggle"
+Toggle.Visible = false
+--Toggle.Parent = Arceus
+Toggle.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+Toggle.Size = UDim2.new(0.95, 0, 0, 50)
+
+UICorner_3.CornerRadius = UDim.new(0.25, 0)
+UICorner_3.Parent = Toggle
+
+Enabled.Name = "Enabled"
+Enabled.Parent = Toggle
+Enabled.AnchorPoint = Vector2.new(1, 0.5)
+Enabled.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+Enabled.Position = UDim2.new(0.975, 0, 0.5, 0)
+Enabled.Size = UDim2.new(0.75, 0, 0.75, 0)
+
+UIAspectRatioConstraint_2.Parent = Enabled
+
+UICorner_4.CornerRadius = UDim.new(0.3, 0)
+UICorner_4.Parent = Enabled
+
+Check.Name = "Check"
+Check.Parent = Enabled
+Check.AnchorPoint = Vector2.new(0.5, 0.5)
+Check.BackgroundColor3 = Color3.fromRGB(255, 74, 74)
+Check.Position = UDim2.new(0.5, 0, 0.5, 0)
+Check.Size = UDim2.new(0.65, 0, 0.65, 0)
+
+UIAspectRatioConstraint_3.Parent = Check
+
+UICorner_5.CornerRadius = UDim.new(0.3, 0)
+UICorner_5.Parent = Check
+
+Name.Name = "Name"
+Name.Parent = Toggle
+Name.AnchorPoint = Vector2.new(0, 0.5)
+Name.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Name.BackgroundTransparency = 1.000
+Name.BorderSizePixel = 0
+Name.Position = UDim2.new(0.057, 0, 0.5, 0)
+Name.Size = UDim2.new(0.75, 0, 0.8, 0)
+Name.Font = Enum.Font.TitilliumWeb
+Name.Text = "Script"
+Name.TextColor3 = Color3.fromRGB(255, 255, 255)
+Name.TextScaled = true
+Name.TextSize = 14.000
+Name.TextWrapped = true
+Name.TextXAlignment = Enum.TextXAlignment.Left
+Name.TextYAlignment = Enum.TextYAlignment.Bottom
+
+UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(180, 180, 180))}
+UIGradient.Parent = Toggle
+
+Button.Name = "Button"
+--Button.Parent = Arceus
+Button.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+Button.Size = UDim2.new(0.95, 0, 0, 50)
+
+UICorner_6.CornerRadius = UDim.new(0.25, 0)
+UICorner_6.Parent = Button
+
+Name_2.Name = "Name"
+Name_2.Parent = Button
+Name_2.AnchorPoint = Vector2.new(0, 0.5)
+Name_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Name_2.BackgroundTransparency = 1.000
+Name_2.BorderSizePixel = 0
+Name_2.Position = UDim2.new(0.05, 0, 0.5, 0)
+Name_2.Size = UDim2.new(0.95, 0, 0.82, 0)
+Name_2.Font = Enum.Font.TitilliumWeb
+Name_2.Text = "Enabled"
+Name_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Name_2.TextScaled = true
+Name_2.TextSize = 14.000
+Name_2.TextWrapped = true
+Name_2.TextXAlignment = Enum.TextXAlignment.Left
+Name_2.TextYAlignment = Enum.TextYAlignment.Bottom
+
+UIGradient_2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(180, 180, 180))}
+UIGradient_2.Parent = Button
+
+tab.Name = "tab"
+tab.Visible = false
+--tab.Parent = Arceus
+tab.BackgroundTransparency = 1
+tab.Size = UDim2.new(0.95, 0, 0.025, 0)
+
+-- SCRIPT
+
+local success, err = pcall(function()
+	Arceus.Parent = uiparent()
+end)
+
+if not success then
+	Arceus.Parent = game:GetService("Players").LocalPlayer.PlayerGui
+end
+
+local TweenService = game:GetService("TweenService")
+local element_height = 50*Menu.AbsoluteSize.Y/210
+local elements = 0
+
+local function uiparent()
+	local success, parent = pcall(function()
+		return gethui()
+	end)
+
+	if not success then
+		return game:GetService("CoreGui")
+	end
+
+	return parent
+end
+
+local function addSpace()
+	local space = tab:Clone()
+	space.Parent = Menu
+	space.LayoutOrder = elements
+	space.Visible = true
+	
+	elements += 1
+end
+
+local function addToggle(name, funct, enabled, ...)
+	local newTog = Toggle:Clone()
+	local args = {...}
+	
+	newTog.MouseButton1Down:Connect(function()
+		enabled = not enabled
+		newTog:WaitForChild("Enabled"):WaitForChild("Check").Visible = enabled
+		funct(enabled, unpack(args))
+	end)
+			
+	newTog:WaitForChild("Enabled"):WaitForChild("Check").Visible = enabled
+	newTog:WaitForChild("Name").Text = name
+	
+	newTog.Size = UDim2.new(0.95, 0, 0, element_height) -- Y1 = 50 : Y2 = X
+	newTog.Name = name
+	newTog.Parent = Menu
+	newTog.LayoutOrder = elements
+	newTog.Visible = true
+	
+	elements += 1
+	addSpace()
+	
+	return newTog
+end
+
+local function addButton(name, funct, ...)
+	local newBut = Button:Clone()
+	local args = {...}
+	
+	newBut.MouseButton1Down:Connect(function()
+		funct(unpack(args))
+	end)
+
+	newBut:WaitForChild("Name").Text = name
+	newBut.Size = UDim2.new(0.95, 0, 0, element_height)
+	newBut.Name = name
+	newBut.Parent = Menu
+	newBut.LayoutOrder = elements
+	newBut.Visible = true
+	
+	elements += 1
+	addSpace()
+	
+	return newBut
+end
+
+-- Actual Script
 
 local speeds = 5
 local nowe = false
 local tpwalking = false
 local speaker = game:GetService("Players").LocalPlayer
 local heartbeat = game:GetService("RunService").Heartbeat
-
--- UI
-
-local MobileFly = Instance.new("ScreenGui")
-local MainFrame = Instance.new("Frame")
-local Toggle = Instance.new("TextButton")
-local Subtract = Instance.new("TextButton")
-local Increment = Instance.new("TextButton")
-local Down = Instance.new("TextButton")
-local Up = Instance.new("TextButton")
-local credits = Instance.new("TextLabel")
-
-MobileFly.Name = "MobileFly"
-MobileFly.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-MainFrame.Name = "MainFrame"
-MainFrame.Parent = MobileFly
-MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
-MainFrame.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
-MainFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-MainFrame.BorderSizePixel = 0
-MainFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
-MainFrame.Size = UDim2.new(0.150000006, 0, 0.150000006, 0)
-MainFrame.Active = true
-MainFrame.Draggable = true
-
-Toggle.Name = "Toggle"
-Toggle.Parent = MainFrame
-Toggle.AnchorPoint = Vector2.new(0.5, 0)
-Toggle.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-Toggle.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Toggle.BorderSizePixel = 0
-Toggle.Position = UDim2.new(0.5, 0, 0.100000001, 0)
-Toggle.Size = UDim2.new(0.5, 0, 0.300000012, 0)
-Toggle.Font = Enum.Font.Sarpanch
-Toggle.Text = "Fly: " .. speeds
-Toggle.TextColor3 = Color3.fromRGB(100, 100, 100)
-Toggle.TextScaled = true
-Toggle.TextSize = 14.000
-Toggle.TextWrapped = true
-
-Subtract.Name = "Subtract"
-Subtract.Parent = MainFrame
-Subtract.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-Subtract.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Subtract.BorderSizePixel = 0
-Subtract.Position = UDim2.new(0.075000003, 0, 0.100000001, 0)
-Subtract.Size = UDim2.new(0.125, 0, 0.300000012, 0)
-Subtract.Font = Enum.Font.Sarpanch
-Subtract.Text = "-"
-Subtract.TextColor3 = Color3.fromRGB(255, 57, 57)
-Subtract.TextScaled = true
-Subtract.TextSize = 14.000
-Subtract.TextWrapped = true
-
-Increment.Name = "Increment"
-Increment.Parent = MainFrame
-Increment.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-Increment.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Increment.BorderSizePixel = 0
-Increment.Position = UDim2.new(0.800000012, 0, 0.100000001, 0)
-Increment.Size = UDim2.new(0.125, 0, 0.300000012, 0)
-Increment.Font = Enum.Font.Sarpanch
-Increment.Text = "+"
-Increment.TextColor3 = Color3.fromRGB(85, 170, 0)
-Increment.TextScaled = true
-Increment.TextSize = 14.000
-Increment.TextWrapped = true
-
-Down.Name = "Down"
-Down.Parent = MainFrame
-Down.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-Down.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Down.BorderSizePixel = 0
-Down.Position = UDim2.new(0.075000003, 0, 0.449999988, 0)
-Down.Size = UDim2.new(0.400000006, 0, 0.300000012, 0)
-Down.Font = Enum.Font.Sarpanch
-Down.Text = "Down"
-Down.TextColor3 = Color3.fromRGB(255, 57, 57)
-Down.TextScaled = true
-Down.TextSize = 14.000
-Down.TextWrapped = true
-
-Up.Name = "Up"
-Up.Parent = MainFrame
-Up.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-Up.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Up.BorderSizePixel = 0
-Up.Position = UDim2.new(0.524999976, 0, 0.449999988, 0)
-Up.Size = UDim2.new(0.400000006, 0, 0.300000012, 0)
-Up.Font = Enum.Font.Sarpanch
-Up.Text = "Up"
-Up.TextColor3 = Color3.fromRGB(85, 170, 0)
-Up.TextScaled = true
-Up.TextSize = 14.000
-Up.TextWrapped = true
-
-credits.Name = "credits"
-credits.Parent = MainFrame
-credits.AnchorPoint = Vector2.new(0, 1)
-credits.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-credits.BackgroundTransparency = 1.000
-credits.BorderColor3 = Color3.fromRGB(0, 0, 0)
-credits.BorderSizePixel = 0
-credits.Position = UDim2.new(0, 0, 1, 0)
-credits.Size = UDim2.new(1, 0, 0.200000003, 0)
-credits.Font = Enum.Font.Kalam
-credits.Text = "Arceus X | Mobile fly remake"
-credits.TextColor3 = Color3.fromRGB(255, 255, 255)
-credits.TextScaled = true
-credits.TextSize = 14.000
-credits.TextWrapped = true
-credits.TextYAlignment = Enum.TextYAlignment.Top
-
--- SCRIPT
-
-local function uiparent()
-    local success, err = pcall(function()
-		return gethui()
-	end)
-	if not success then
-		return speaker.PlayerGui
-	end
-	return err;
-end
 
 local function updatespeed(char, hum)
 	if nowe == true then
@@ -158,17 +313,26 @@ local function updatespeed(char, hum)
 	end
 end
 
-Toggle.MouseButton1Down:Connect(function()
+speaker.CharacterAdded:Connect(function(char)
+	local char = speaker.Character
+	if char then
+		task.wait(0.7)
+		char.Humanoid.PlatformStand = false
+		char.Animate.Disabled = false
+	end
+end)
+
+local toggleBtn
+toggleBtn = addToggle("Toggle fly: " .. speeds, function(enabled)
 	local char = speaker.Character
 	if not char or not char.Humanoid then
 		return
 	end
-	
+
 	local hum = char.Humanoid
 	if nowe == true then
 		nowe = false
-		Toggle.TextColor3 = Color3.fromRGB(100, 100, 100)
-
+		
 		hum:SetStateEnabled(Enum.HumanoidStateType.Climbing,true)
 		hum:SetStateEnabled(Enum.HumanoidStateType.FallingDown,true)
 		hum:SetStateEnabled(Enum.HumanoidStateType.Flying,true)
@@ -187,15 +351,13 @@ Toggle.MouseButton1Down:Connect(function()
 		hum:ChangeState(Enum.HumanoidStateType.RunningNoPhysics)	
 	else
 		nowe = true
-		Toggle.TextColor3 = Color3.fromRGB(255, 255, 255)
-
 		updatespeed(char, hum)
-		
+
 		char.Animate.Disabled = true
 		for i,v in next, hum:GetPlayingAnimationTracks() do
 			v:AdjustSpeed(0)
 		end
-		
+
 		hum:SetStateEnabled(Enum.HumanoidStateType.Climbing,false)
 		hum:SetStateEnabled(Enum.HumanoidStateType.FallingDown,false)
 		hum:SetStateEnabled(Enum.HumanoidStateType.Flying,false)
@@ -226,15 +388,15 @@ Toggle.MouseButton1Down:Connect(function()
 	bg.P = 9e4
 	bg.maxTorque = Vector3.new(9e9, 9e9, 9e9)
 	bg.cframe = UpperTorso.CFrame
-	
+
 	local bv = Instance.new("BodyVelocity", UpperTorso)
 	bv.velocity = Vector3.new(0,0.1,0)
 	bv.maxForce = Vector3.new(9e9, 9e9, 9e9)
-	
+
 	if nowe == true then
 		hum.PlatformStand = true
 	end
-	
+
 	while nowe == true or hum.Health == 0 do
 		task.wait()
 
@@ -260,65 +422,79 @@ Toggle.MouseButton1Down:Connect(function()
 
 		bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0)
 	end
-	
+
 	ctrl = {f = 0, b = 0, l = 0, r = 0}
 	lastctrl = {f = 0, b = 0, l = 0, r = 0}
 	speed = 0
 	bg:Destroy()
 	bv:Destroy()
-	
+
 	hum.PlatformStand = false
 	char.Animate.Disabled = false
 	tpwalking = false
+end, false)
+
+local incraseBtn
+incraseBtn = addButton("Incrase speed", function()
+	local char = speaker.Character
+	if char and char.Humanoid then
+		local hum = char.Humanoid
+
+		speeds = speeds + 1
+		updatespeed(char, hum)
+		
+		toggleBtn:WaitForChild("Name").Text = "Toggle fly: " .. speeds
+	end
 end)
 
-Up.MouseButton1Down:connect(function()
+local decraseBtn
+decraseBtn = addButton("Decrase speed", function()
+	local char = speaker.Character
+	if char and char.Humanoid then
+		local hum = char.Humanoid
+
+		if speeds > 1 then
+			speeds = speeds - 1
+			updatespeed(char, hum)
+		end
+
+		toggleBtn:WaitForChild("Name").Text = "Toggle fly: " .. speeds
+	end
+end)
+
+local goUp
+goUp = addButton("Go up", function()
 	local char = speaker.Character
 	if char then
 		char.HumanoidRootPart.CFrame = char.HumanoidRootPart.CFrame * CFrame.new(0,2,0)
 	end
 end)
 
-Down.MouseButton1Down:connect(function()
+local goDown
+goDown = addButton("Go down", function()
 	local char = speaker.Character
 	if char then
 		char.HumanoidRootPart.CFrame = char.HumanoidRootPart.CFrame * CFrame.new(0,-2,0)
 	end
 end)
 
-speaker.CharacterAdded:Connect(function(char)
-	local char = speaker.Character
-	if char then
-		task.wait(0.7)
-		char.Humanoid.PlatformStand = false
-		char.Animate.Disabled = false
-	end
-end)
+-- INIT
 
-Increment.MouseButton1Down:connect(function()
-	local char = speaker.Character
-	if char and char.Humanoid then
-		local hum = char.Humanoid
-		
-		speeds = speeds + 1
-		Toggle.Text = "Fly: " .. speeds
-		
-		updatespeed(char, hum)
-	end
-end)
+Main:TweenPosition(
+	UDim2.fromScale(0.5, 0.5),
+	Enum.EasingDirection.In,
+	Enum.EasingStyle.Quad,
+	1, false, nil
+)
 
-Subtract.MouseButton1Down:connect(function()
-	local char = speaker.Character
-	if char and char.Humanoid then
-		local hum = char.Humanoid
-		
-		if speeds > 1 then
-			speeds = speeds - 1
-			updatespeed(char, hum)
-		end
-	
-		Toggle.Text = "Fly: " .. speeds
-	end
-end)
+task.wait(1.5)
+Logo:TweenSizeAndPosition(
+	UDim2.fromScale(0.175, 0.175),
+	UDim2.fromScale(0.075, 0.15),
+	Enum.EasingDirection.In,
+	Enum.EasingStyle.Quad,
+	1, false, nil
+)
 
-MobileFly.Parent = uiparent()
+task.wait(1.5)
+TweenService:Create(Intro, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {BackgroundTransparency = 1}):Play()
